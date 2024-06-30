5ddd96f24f129571d10b5923ebece2d416494358
Shagnik Biswas <78656656+Shagnik-Biswas@users.noreply.github.com>
2024-06-30 20:33:47 +0530

    stone_paper_scissor.py
    A Traditional Game Of Stone-Paper-Scissor Between Two Players.

--- stone_paper_scissor.py
+++ stone_paper_scissor.py
@@ -8,3 +8,36 @@
 # Stone Wins against Scissor BUT Loses to Paper.
 # Paper Wins against Stone BUT Loses to Scissor.
 # Scissor Wins against Paper but Loses to Stone.
+# If Two Player draw same items between each other, it will be considered a Draw.
+
+player1 = ["stone","paper","scissor","stone", "paper","stone","paper","paper","stone","paper"]
+player2 = ["paper","stone", "paper","paper","scissor", "scissor","stone","stone","paper","scissor"]
+
+winner = []
+
+for i in range(len(player1)):
+    if winner.count('player1') < 5 and winner.count('player2') < 5:
+        if player1[i] != player2[i]:
+          if player1[i] == 'stone':
+              if player2[i] == 'paper':
+                  winner += ['player2']
+              else:
+                  winner += ['player1']
+          elif player1[i] == 'paper':
+              if player2[i] == 'scissor':
+                  winner += ['player2']
+              else:
+                  winner += ['player1']
+          elif player1[i] == 'scissor':
+              if player2[i] == 'stone':
+                  winner += ['player2']
+              else:
+                  winner += ['player1']
+
+player1_round = winner.count('player1')
+player2_round = winner.count('player2')
+
+if player1_round == player2_round:
+    print(f'It\'s a Draw. Total {len(winner)} Round Of Games Played. Both Player Won {player1_round} rounds.')
+else:
+    print(f'Total {len(winner)} Round Of Games Played. Player1 Won {player1_round} round and Player2 Won {player2_round}. So The Winner is: {max(winner, key=winner.count)}.')
