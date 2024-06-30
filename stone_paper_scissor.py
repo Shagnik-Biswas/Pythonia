bf4ef61d81d6b2c111cd3e8c40728082cdede398
Shagnik Biswas <78656656+Shagnik-Biswas@users.noreply.github.com>
2024-06-30 20:23:21 +0530

    stone_paper_scissor.py

--- stone_paper_scissor.py
+++ stone_paper_scissor.py
@@ -2,3 +2,4 @@
 # This is a game of stone paper scissor. There will be two player joining this game. Each can play any round of game. If they play more than 5 round of game then the first player to win 5 round will be the WINNER.
 # Otherwise if they play less than 5 rounds then whoever wins the most round between them will be the winner.
 # Players can stop at any round and if they won equal round of game, then the game will be considered as a DRAW.
+# Each Player will have equal amout of rounds as this is the game's rule.
